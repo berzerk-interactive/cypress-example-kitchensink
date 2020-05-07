5015d149c682411c57552eae53e48c95c53db7bc
Lee <leeblazek@gmail.com>
2020-05-07 12:38:12 -0600

    add host param

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,71 +1,79 @@
 /// <reference types="cypress" />
 
-context('Actions', () => {
+context("Actions", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/commands/actions')
-  })
+    cy.visit(`http://${Cypress.env("host")}commands/actions`);
+  });
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('.type() - type into a DOM element', () => {
+  it(".type() - type into a DOM element", () => {
     // https://on.cypress.io/type
-    cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+    cy.get(".action-email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com")
 
       // .type() with special character sequences
-      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
-      .type('{del}{selectall}{backspace}')
+      .type("{leftarrow}{rightarrow}{uparrow}{downarrow}")
+      .type("{del}{selectall}{backspace}")
 
       // .type() with key modifiers
-      .type('{alt}{option}') //these are equivalent
-      .type('{ctrl}{control}') //these are equivalent
-      .type('{meta}{command}{cmd}') //these are equivalent
-      .type('{shift}')
+      .type("{alt}{option}") //these are equivalent
+      .type("{ctrl}{control}") //these are equivalent
+      .type("{meta}{command}{cmd}") //these are equivalent
+      .type("{shift}")
 
       // Delay each keypress by 0.1 sec
-      .type('slow.typing@email.com', { delay: 100 })
-      .should('have.value', 'slow.typing@email.com')
+      .type("slow.typing@email.com", { delay: 100 })
+      .should("have.value", "slow.typing@email.com");
 
-    cy.get('.action-disabled')
+    cy.get(".action-disabled")
       // Ignore error checking prior to type
       // like whether the input is visible or disabled
-      .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
-  })
+      .type("disabled error checking", { force: true })
+      .should("have.value", "disabled error checking");
+  });
 
-  it('.focus() - focus on a DOM element', () => {
+  it(".focus() - focus on a DOM element", () => {
     // https://on.cypress.io/focus
-    cy.get('.action-focus').focus()
-      .should('have.class', 'focus')
-      .prev().should('have.attr', 'style', 'color: orange;')
-  })
+    cy.get(".action-focus")
+      .focus()
+      .should("have.class", "focus")
+      .prev()
+      .should("have.attr", "style", "color: orange;");
+  });
 
-  it('.blur() - blur off a DOM element', () => {
+  it(".blur() - blur off a DOM element", () => {
     // https://on.cypress.io/blur
-    cy.get('.action-blur').type('About to blur').blur()
-      .should('have.class', 'error')
-      .prev().should('have.attr', 'style', 'color: red;')
-  })
+    cy.get(".action-blur")
+      .type("About to blur")
+      .blur()
+      .should("have.class", "error")
+      .prev()
+      .should("have.attr", "style", "color: red;");
+  });
 
-  it('.clear() - clears an input or textarea element', () => {
+  it(".clear() - clears an input or textarea element", () => {
     // https://on.cypress.io/clear
-    cy.get('.action-clear').type('Clear this text')
-      .should('have.value', 'Clear this text')
+    cy.get(".action-clear")
+      .type("Clear this text")
+      .should("have.value", "Clear this text")
       .clear()
-      .should('have.value', '')
-  })
+      .should("have.value", "");
+  });
 
-  it('.submit() - submit a form', () => {
+  it(".submit() - submit a form", () => {
     // https://on.cypress.io/submit
-    cy.get('.action-form')
-      .find('[type="text"]').type('HALFOFF')
-    cy.get('.action-form').submit()
-      .next().should('contain', 'Your form has been submitted!')
-  })
+    cy.get(".action-form").find('[type="text"]').type("HALFOFF");
+    cy.get(".action-form")
+      .submit()
+      .next()
+      .should("contain", "Your form has been submitted!");
+  });
 
-  it('.click() - click on a DOM element', () => {
+  it(".click() - click on a DOM element", () => {
     // https://on.cypress.io/click
-    cy.get('.action-btn').click()
+    cy.get(".action-btn").click();
 
     // You can click on 9 specific positions of an element:
     //  -----------------------------------
